00ddd1a52620896bfdee5e715f49de0149ef0b7e
Emily Menezes <menezesemily2008@gmail.com>
2025-02-06 14:52:20 -0300

    Método AplicarCor() criado

--- obj/Debug/net8.0/atividade.AssemblyInfo.cs
+++ obj/Debug/net8.0/atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbbe884b28835c9d412672f0882325b2de814d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab15a22e9249ea31a2719cfec47e8a3b76de0889")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
